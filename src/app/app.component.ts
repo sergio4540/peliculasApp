41c8e6b15c2cd5e566e428d4cfd14b8d75d770d5
sergio4540 <sergio4540@hotmail.com>
2022-01-28 17:07:50 -0500

    feat: create pelicula-service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PeliculasService } from './services/peliculas.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'peliculasApp';
+
+  constructor(private peliculasServices: PeliculasService){
+    this.peliculasServices.getCartelera().subscribe(resp => {
+      console.log(resp);
+    })
+  }
 }
