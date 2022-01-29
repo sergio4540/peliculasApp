03f23d80aefb3641793b9902e40fd63f41e35485
sergio4540 <sergio4540@hotmail.com>
2022-01-29 15:33:09 -0500

    feat: peliculas-poster-grid

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,21 +3,26 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { RatingModule } from 'ng-starrating';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    PeliculasPosterGridComponent
   ],
   exports:[
     NavbarComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    PeliculasPosterGridComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    RatingModule
   ]
 })
 export class ComponentsModule { }
